3328f206af5fb30e15ce36b11fe7bc1ebfd51bc2
Adriel <adriel.mounzon@ucb.edu.bo>
2023-03-27 22:08:07 -0400

    Refactorización de código

--- src/sumadorDeCadenas.js
+++ src/sumadorDeCadenas.js
@@ -1,10 +1,15 @@
-function sumarCadena(cadena)
+function convertirNumero(cadena)
 {
   if (cadena == "")
   {
     return 0;
   }
   return parseInt(cadena);
+}
+
+function sumarCadena(cadena)
+{
+  return convertirNumero(cadena);
 }
 
 export default sumarCadena;
